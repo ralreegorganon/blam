8405fa89cb4bebc92fb72884c9267d7ba0ddf699
Jason Jones <jasonedwardjones@gmail.com>
2014-05-29 20:07:42 -0800

    Report what I'm doing...

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var directory = flag.String("directory", ".", "Directory to serve")
 
 func main() {
 	flag.Parse()
+	log.Printf("Serving %v at http://localhost:%v", *directory, *port)
 	err := http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*directory)))
 	if err != nil {
 		log.Fatal(err)
